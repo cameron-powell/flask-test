bab9b3d3a7779788aa4ebde595020a62bc586325
Cameron <cameronspowell@gmail.com>
2017-02-26 23:50:35 -0500

    Adds users to database

--- src/main.py
+++ src/main.py
@@ -1,42 +1,36 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-import os
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-#from flask_script import Manager
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-# database setup
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
-# wtforms csrf protection
-app.config['SECRET_KEY'] = 'hard to guess string' #TODO: Change this for production (store in environment variable instead of embedded in code)
-#manager = Manager(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
-        return'<Role %r>' % self.name
+        return '<Role %r>' % self.name
 
 
 class User(db.Model):
@@ -49,47 +43,14 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@app.route('/useragent')
-def useragent():
-    from flask import request
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s' %user_agent
-
-
-@app.route('/badrequest')
-def badrequest():
-    return '<h1>Bad Request</h1>', 400
-
-
-@app.route('/responseobject')
-def responseobject():
-    from flask import make_response
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
-
-
-@app.route('/redir')
-def redir():
-    from flask import redirect
-    return redirect('http://www.google.com')
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @app.errorhandler(404)
@@ -102,6 +63,22 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    #manager.run()
+    manager.run()
